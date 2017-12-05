ed65c676c442fba68df202dbdee5d0323fe8df52
ppmajchrzak@gmail.com <ppmajchrzak@gmail.com>
2017-12-05 20:23:42 +0100

    arrays

--- NyTimesQuery.php
+++ NyTimesQuery.php
@@ -7,7 +7,44 @@
  * Time: 22:32
  */
 
+function search()
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $query = array(
+        "api-key" => "07eb75aec08549dba5be57881986ae90",
+        "q" => $_POST['phrase'],
+        "sort" => "newest",
+        "fl" => "web_url, snippet, multimedia, headline, pub_date,",
+        "page" => 0
+    );
+    curl_setopt($curl, CURLOPT_URL,
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json" . "?" . http_build_query($query)
+    );
+    $result = json_decode(curl_exec($curl));
+    $array = [];
+
+    for ($i = 0; $i <= 9; $i++) {
+        $array['web_url'][] = $result->response->docs[$i]->web_url;
+        $array['snippet'][] = $result->response->docs[$i]->snippet;
+        $array['pub_date'][] = $result->response->docs[$i]->pub_date;
+        $array['main'][] =  $result->response->docs[$i]->headline->main;
+      if ($result->response->docs[$i]->multimedia != null) {
+          $array['thumbnail_url'][] = $result->response->docs[$i]->multimedia[2]->url;;
+        } else {
+          $array['thumbnail_url'][] = null;
+      }
+    }
+    print_r ($array);
+    return $result;
+
+}
+$merkel = search();
+var_dump($merkel->response);
+
+
 require_once "NyTimesMention.php";
+
 
 class NyTimesQuery implements NyTimesMention
 {
@@ -63,9 +100,9 @@ class NyTimesQuery implements NyTimesMention
 
 }
 
-$obama = new NyTimesQuery();
-$obama->search();
-var_dump($obama);
+//$obama = new NyTimesQuery();
+//$obama->search();
+//var_dump($obama);
 
 
 
